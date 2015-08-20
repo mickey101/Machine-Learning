bde9287b0fa19fdeb0114680e49ab998ceebfe06
Emiwodew Gizaw <egizaw@jacobs-university.de>
2015-08-21 00:05:15 +0200

    Machine learning problems
    This are the problems given by the course and it includes my solutions
    to them

---
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,22 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+% g = 1./(1 + exp.(-z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+len = size(z,2); 
+dep = size(z,1); 
+ for i = 1:len
+     for j = 1:dep
+       g(j,i) = 1/(1 + exp(-z(j,i))); 
+     end
+ end
+
+% =============================================================
+end
